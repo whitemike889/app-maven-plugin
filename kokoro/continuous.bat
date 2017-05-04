90b13b51152f80ed879ccfbb80f15f6dafff7298
Andras Kerekes <akerekes@users.noreply.github.com>
2017-05-04 13:27:46 -0400

    Kokoro release setup (#184)
    * initial attempt for build release step
    
    * copy pom to target with new name
    
    * avoid mvn download progress output
    
    * test gsutil
    
    * add sign job
    
    * tweak path to unsigned jars
    
    * fix build script path
    
    * fix path for find
    
    * exit with error if signing a file fails
    
    * remove invalid argument from signing command
    
    * debug: ls to see what files have been created
    
    * add signature files to artifacts
    
    * debug: ls to see what files have been created
    
    * create bundle.jar
    
    * add bundle.jar to artifacts
    
    * build bundle.jar w/o internal directory structure
    
    * move artifacts to separate directory before signing
    
    * use absolute path to specify which file to sign
    
    * code cleanup
    
    * change the bundle name to reflect plugin name and version
    
    * fix bash string substitution
    
    * Update continuous.bat due to changes in Kokoro
    
    Kokoro Windows now has cloud sdk

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -1,16 +1,9 @@
 cd github/app-maven-plugin
 
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe
-start /WAIT GoogleCloudSDKInstaller.exe /S /noreporting /nostartmenu /nodesktop /logtofile /D=T:\google
-call t:\google\google-cloud-sdk\bin\gcloud.cmd components copy-bundled-python>>python_path.txt && SET /p CLOUDSDK_PYTHON=<python_path.txt && DEL python_path.txt
-call t:\google\google-cloud-sdk\bin\gcloud.cmd components update --quiet
-call t:\google\google-cloud-sdk\bin\gcloud.cmd components install app-engine-java --quiet
-set GOOGLE_CLOUD_SDK_HOME=t:\google\google-cloud-sdk
+call gcloud.cmd components update --quiet
+call gcloud.cmd components install app-engine-java --quiet
 
-wget http://www-us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
-unzip apache-maven-3.3.9-bin.zip
-
-apache-maven-3.3.9/bin/mvn clean install cobertura:cobertura -B -U
+mvn clean install cobertura:cobertura -B -U
 REM curl -s https://codecov.io/bash | bash
 
 exit /b %ERRORLEVEL%
---
+++ kokoro/ubuntu/release_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands to stderr.
+set -x
+
+cd github/app-maven-plugin
+
+sudo /opt/google-cloud-sdk/bin/gcloud components update
+sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
+
+mvn -Prelease -B -U verify
+
+# copy pom with the name expected in the Maven repository
+ARTIFACT_ID=$(mvn -B help:evaluate -Dexpression=project.artifactId 2>/dev/null | grep -v "^\[")
+PROJECT_VERSION=$(mvn -B help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[")
+cp pom.xml target/${ARTIFACT_ID}-${PROJECT_VERSION}.pom
+
