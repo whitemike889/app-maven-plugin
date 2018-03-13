552d5e906397ac8c41a049e4abfec5648670b272
Tad Cordle <tad.cordle@gmail.com>
2018-03-13 11:32:07 -0400

    Force Java 8 on Windows kokoro builds (#246)

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -1,3 +1,7 @@
+REM Java 9 does not work with our build at the moment, so force java 8
+set JAVA_HOME=c:\program files\java\jdk1.8.0_152
+set PATH=%JAVA_HOME%\bin;%PATH%
+
 cd github/app-maven-plugin
 
 call gcloud.cmd components update --quiet
