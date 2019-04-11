dd8afb9071813cc022f8d757698e8aeb5303d991
Appu <appu@google.com>
2019-04-11 15:52:32 -0400

    Kokoro macos temporary fix - use newer JDK (#380)
    * Use adopt openjdk 8u202 for macos tests (temporary workaround)

--- kokoro/continuous_mac.sh
+++ kokoro/continuous_mac.sh
@@ -8,6 +8,12 @@ set -x
 gcloud components update
 gcloud components install app-engine-java
 
+# Use adopt openjdk 8u202 until kokoro updates its macos images (b/130225695)
+wget https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u202-b08/OpenJDK8U-jdk_x64_mac_hotspot_8u202b08.tar.gz
+tar -xzf OpenJDK8U-jdk_x64_mac_hotspot_8u202b08.tar.gz
+
+JAVA_HOME="$(pwd)"/jdk8u202-b08/Contents/Home/
+
 # temporary workaround until mvn is available in the image by default
 # the integration tests rely on Maven being installed, cannot use the wrapper
 curl  https://storage.googleapis.com/cloud-tools-for-java-team-kokoro-build-cache/apache-maven-3.5.0-bin.tar.gz -o apache-maven-3.5.0-bin.tar.gz
